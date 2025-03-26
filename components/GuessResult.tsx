27478dc0c902ec36f2a00afded66779ff8a5e5bc
Elliot <elliot@elliots-mbp.home>
2025-03-26 20:53:23 +0000

    Fix GuessResponseWithValues import in GuessResult component

--- components/GuessResult.tsx
+++ components/GuessResult.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GuessResponseWithValues } from '../services/gameService';
+import { GuessResponseWithValues } from '../types/player';
 
 interface GuessResultProps {
   guess: GuessResponseWithValues;
