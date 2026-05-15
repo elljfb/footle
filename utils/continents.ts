97ebb164950c815437014ce17a1ee72706cdc514
elljfb <ebriggs1995@gmail.com>
2026-05-15 11:13:21 +0100

    fix: dr congo and ivory coast

--- utils/continents.ts
+++ utils/continents.ts
@@ -25,9 +25,9 @@ const continentMapping: ContinentMap = {
   'Africa': [
     'Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 
     'Cameroon', 'Cape Verde', 'Central African Republic', 'Chad', 'Comoros', 
-    'Congo', 'Democratic Republic of the Congo', 'Djibouti', 'Egypt', 
+    'Congo', 'DR Congo', 'Djibouti', 'Egypt', 
     'Equatorial Guinea', 'Eritrea', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 
-    'Guinea', 'Guinea-Bissau', 'Ivory Coast', 'Kenya', 'Lesotho', 'Liberia', 
+    'Guinea', 'Guinea-Bissau', 'Cote dIvoire', 'Kenya', 'Lesotho', 'Liberia', 
     'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 
     'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 
     'Senegal', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 
